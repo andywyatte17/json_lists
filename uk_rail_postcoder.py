cc1d76887d344c9e2151dbd4f4e045c919774d9f
Andrew Wyatt <andywyatte17@gmail.com>
2018-02-27 23:23:05 +0000

    Updated uk_rail_stations_filtered.csv

--- uk_rail_postcoder.py
+++ uk_rail_postcoder.py
@@ -3,26 +3,25 @@ import csv
 c1 = csv.reader(open('uk_rail_stations_2011-12_filtered.csv', 'rb'))
 c2 = csv.writer(open('uk_rail_stations_2011-12_filtered_2.csv', 'wb'))
 
-import re
-from postcodes import PostCoder
-pc = PostCoder()
+from geopy.geocoders import Nominatim # python -m pip install --user geopy
 
-def filter_postcode(x):
-    m = re.match(r'([A-Z]+[0-9]+) ([0-9]+[A-Z]+)', x)
-    x = m.group(1) + ' ' + m.group(2)
-    return x
-    
+def postcodeToGeo(x):
+    geolocator = Nominatim()
+    location = geolocator.geocode(x)
+    return "{},{}".format(location.longitude, location.latitude)
+
 row = 0
 for x in c1:
     row += 1
     long_lati = "long_lati"
     if row>1:
         try:
-            # result = pc.get(filter_postcode(x[3]))
-            #lat = result['geo']['lat']
-            #lng = result['geo']['lng']
-            #long_lati = lng + "," + lat 
-            long_lati = "filter_postcode(x[3])"
+            long_lati = x[2]
+            if x[2]=="?":
+                result = postcodeToGeo(x[3])
+                long_lati = result
+                print(result, row)
         except:
             long_lati = "?"
+    x[2] = long_lati
     c2.writerow(x)
